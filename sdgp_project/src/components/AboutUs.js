1c4bf7885ba388b90a3ee76ee24410c6557f8307
Ashwinth <66359270+Razorzaldin@users.noreply.github.com>
2021-02-28 19:10:27 +0530

    Component and Bootstrap
    Added component and bootstrap for About Us page

---
+++ sdgp_project/src/components/AboutUs.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {About} from './About';
+import {Layout} from './Layout';
+
+class AboutUs extends Component {
+    state = {  }
+    render() { 
+        return ( 
+          <Layout>
+             <About>
+             </About>
+          </Layout>
+
+         );
+    }
+}
+ 
+export default AboutUs;
