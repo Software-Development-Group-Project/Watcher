ed7ff200fb543995af25d6a34471dd588966d91a
HananCS <benhanan.subendran@gmail.com>
2021-03-15 15:57:06 +0530

    changes on webcam

--- sdgp_project/src/components/Mask.js
+++ sdgp_project/src/components/Mask.js
@@ -6,20 +6,35 @@ import Webcam from "react-webcam";
 class Mask extends Component {
     render() { 
         
-        
+        const videoConstraints = {
+            width: 1280,
+            height: 720,
+            facingMode: "user"
+          };
 
-    const WebcamComponent = () => <Webcam />;
-    
+        
         return ( 
                 <div>
                     <img src = {img} alt="alert"/> 
                     <p> Check People</p>
                     <p>This web cam can check whether you're wearing mask or not</p>
-                    <WebcamComponent/>
+                    <Webcam
+                        audio={false}
+                        height={720}
+                        // ref={webcamRef}
+                        screenshotFormat="image/jpeg"
+                        width={1280}
+                        videoConstraints={videoConstraints}
+                    />
+                    <button >Capture photo</button>
                 </div>
         );
+          
+          
+        
  
     }
 }
+
  
 export default Mask;
