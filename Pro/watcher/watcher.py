59adde63386b7e9362510c4d8db3ec91b39dba83
S-Nirahulan <nirahulan.20191022@iit.ac.lk>
2021-04-15 18:00:32 +0530

    debug set to true

--- Pro/watcher/watcher.py
+++ Pro/watcher/watcher.py
@@ -3,18 +3,22 @@ from camera import Camera
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+
 @app.route('/help')
 def help():
     return render_template('help.html')
 
+
 @app.route('/live')
 def live():
     return render_template('live.html')
+
 
 def gen(camera):
     while True:
@@ -22,9 +26,11 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
