508675de378ecad0f4825e6b0d6a55f617e15b79
S-Nirahulan <nirahulan.20191022@iit.ac.lk>
2021-04-19 14:45:26 +0530

    New class for mail server

---
+++ Pro/watcher/mail_server.py
@@ -0,0 +1,36 @@
+import imghdr
+import smtplib
+from email.message import EmailMessage
+import threading
+
+password = "jsexodmjokkpcebg"
+user = "watchera2019@gmail.com"
+admin = "nirahulan.20191022@iit.ac.lk"
+
+
+class MailServer:
+
+    def __init__(self):
+        self.server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+
+    def __del__(self):
+        self.server.quit()
+
+    def send_email(self, subject, message, to):
+
+        msg = EmailMessage()
+        msg.set_content(message)
+        msg['subject'] = subject
+        msg['to'] = to
+        msg['from'] = user
+
+        self.server.login(user, password)
+        self.server.send_message(msg)
+
+    def create_mail(self, name, to):
+        heading = " Watcher (alert) "
+        message = "{}, put your mask immediately".format(name.capitalize())
+        adminMessage = "{} is not wearing a mask, please check".format(name.capitalize())
+
+        self.send_email(heading, message, to)
+        self.send_email(heading, adminMessage, admin)
