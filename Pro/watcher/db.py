ab1e607dcb6f38f9cf870e7499321cea2ff2f138
S-Nirahulan <nirahulan.20191022@iit.ac.lk>
2021-04-20 10:43:15 +0530

    reformat the db.py

--- Pro/watcher/db.py
+++ Pro/watcher/db.py
@@ -3,8 +3,7 @@ import sqlite3
 con = sqlite3.connect("members.db")  
 print("Database opened successfully")  
   
-con.execute("create table Members (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, address TEXT NOT NULL, number INTEGER NOT NULL)")  
-  
+con.execute("create table Members (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, address TEXT NOT NULL, number INTEGER NOT NULL)")
 print("Table created successfully")  
   
 con.close()  
